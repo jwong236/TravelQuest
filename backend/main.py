ef82446d56801504be25b3e5f4fade0e9f18026b
Patrick <pattywong2882@gmail.com>
2024-01-27 20:21:00 -0800

    Connect frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,20 @@
 from tourist import TouristClass
 from utils.quest import Quest
 from quester import Quester
+from flask import Flask, jsonify
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
 def main1():
     t = TouristClass()
     t.fetch_current_location()
     print(t.longitude)
     print(t.latitude)
-    print(t.get_nearby_poi())
+    # print(t.get_nearby_poi())
+    return jsonify(t.get_nearby_poi())
 
 def main2():
     q = Quester()
