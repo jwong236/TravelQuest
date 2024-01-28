f3d7186ccbd5593cd0702971f0f2b5d904a6e852
FustinCS <FustinCS@gmail.com>
2024-01-27 16:22:22 -0800

    Add home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import './app.css';
 
 function App() {
   return <>
-    <div>Hello World!
-    </div>
+  <div className="homescreen-container" >
+    <h1 className="home-title">TravelQuest</h1>
+    <p className="home-description">Embark on your quest to travel around the world!</p>
+    <TextField className="home-search" id="outlined-basic" label="Enter an Address" variant="outlined"/>
+    <form id="image_file" onSubmit={this.handleSubmit}>
+      <TextField className = "form-field" type="file"/>
+      <Button className = "form-button" variant="contained" color="primary" component="span" type="submit">Upload</Button>
+    </form>
+  </div>
   </>
 }
-
+  
 export default App
