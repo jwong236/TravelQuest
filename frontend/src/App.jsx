1b0a1e96bcb1d2b8da51d0bafd0790514f1e6aa6
Patrick <pattywong2882@gmail.com>
2024-01-27 20:58:39 -0800

    Updated test function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,26 +5,33 @@ import Box from '@mui/material/Box';
 import HomeIcon from '@mui/icons-material/Home';
 import './app.css';
 
+function test() {
+  console.log("Hello");
+  fetch("http://localhost:5000")
+  .then(res => res.json())
+  .then(resp => {console.log(resp)});
+}
+
 function App() {
   return <>
-  <div className="homescreen-container" >
-    <h1 className="home-title">TravelQuest</h1>
-    <p className="home-description">Embark on your quest to travel around the world!</p>
-    <Box sx={{ display: 'flex', alignItems: 'center', gap: '24px'}}>
-      <HomeIcon fontSize='large'/>
-      <TextField className="home-search" id="outlined-basic" label="Address" variant="outlined" InputLabelProps={{ style: { color: 'white' } }} sx={{
-    fieldset: { borderColor: "white"}
-  
-  }}/>
-    </Box>
-    <form id="image_file">
-      <TextField className = "form-field" type="file" InputLabelProps={{ style: { color: 'white'} }} sx={{
-    fieldset: { borderColor: "white"}
-  }}/>
-      <Button className = "form-button" variant="contained" color="primary" component="span" type="submit">Upload</Button>
+    <div className="homescreen-container" >
+      <h1 className="home-title">TravelQuest</h1>
+      <p className="home-description">Embark on your quest to travel around the world!</p>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: '24px' }}>
+        <HomeIcon fontSize='large' />
+        <TextField className="home-search" id="outlined-basic" label="Address" variant="outlined" InputLabelProps={{ style: { color: 'white' } }} sx={{
+          fieldset: { borderColor: "white" }
+
+        }} />
+      </Box>
+      <form id="image_file">
+        <TextField className="form-field" type="file" InputLabelProps={{ style: { color: 'white' } }} sx={{
+          fieldset: { borderColor: "white" }
+        }} />
+        <Button className="form-button" variant="contained" color="primary" component="span" type="submit" onClick={test}>Upload</Button>
     </form>
-  </div>
+  </div >
   </>
 }
-  
+
 export default App
