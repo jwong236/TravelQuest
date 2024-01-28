3ee0bd52181f9eef7c6094a4cfdd61734a5e3524
FustinCS <FustinCS@gmail.com>
2024-01-27 18:42:13 -0800

    Add home page functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import HomeIcon from '@mui/icons-material/Home';
 import './app.css';
 
 function App() {
@@ -8,9 +10,17 @@ function App() {
   <div className="homescreen-container" >
     <h1 className="home-title">TravelQuest</h1>
     <p className="home-description">Embark on your quest to travel around the world!</p>
-    <TextField className="home-search" id="outlined-basic" label="Enter an Address" variant="outlined"/>
-    <form id="image_file" onSubmit={this.handleSubmit}>
-      <TextField className = "form-field" type="file"/>
+    <Box sx={{ display: 'flex', alignItems: 'center', gap: '24px'}}>
+      <HomeIcon fontSize='large'/>
+      <TextField className="home-search" id="outlined-basic" label="Address" variant="outlined" InputLabelProps={{ style: { color: 'white' } }} sx={{
+    fieldset: { borderColor: "white"}
+  
+  }}/>
+    </Box>
+    <form id="image_file">
+      <TextField className = "form-field" type="file" InputLabelProps={{ style: { color: 'white'} }} sx={{
+    fieldset: { borderColor: "white"}
+  }}/>
       <Button className = "form-button" variant="contained" color="primary" component="span" type="submit">Upload</Button>
     </form>
   </div>
